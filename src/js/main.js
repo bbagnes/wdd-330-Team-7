28db7348841d67dbbd7b577965295f0c9e28b86c
ray24004 <wraymundo@byupathway.edu>
2025-05-19 22:33:33 -0300

    Steps 4.5 and 4.6

--- src/js/main.js
+++ src/js/main.js
@@ -1,3 +1,11 @@
-import { ProductData } from './ProductData.mjs';
+import ProductData from './ProductData.mjs';
+import ProductList from './ProductList.mjs';
+
+const productListElement = document.querySelector('.product-list');
 
 const productData = new ProductData();
+const productList = new ProductList(
+  'category',
+  productData,
+  productListElement,
+);
