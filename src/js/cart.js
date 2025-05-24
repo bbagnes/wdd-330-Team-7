9691417e52724fbb5ce759e7e3d438f2290194c9
ray24004 <wraymundo@byupathway.edu>
2025-05-24 00:41:43 -0300

    Fix cart list rendering

--- src/js/cart.js
+++ src/js/cart.js
@@ -3,6 +3,6 @@ import { loadHeaderFooter } from './utils.mjs';
 
 loadHeaderFooter();
 
-const cartListElement = document.getElementById('cart-list');
+const cartListElement = document.querySelector('.product-list');
 const cart = new ShoppingCart(cartListElement, 'cart-template');
 cart.displayCart();
