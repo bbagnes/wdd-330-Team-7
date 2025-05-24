5d7d4e9fa61dc45b13a13130e03fb9c7e5d4e3f9
bbagnes <bbagnes@byupathway.edu>
2025-05-23 21:08:21 -0600

    commented out console.log call creating lint warning.

--- src/js/product.js
+++ src/js/product.js
@@ -4,7 +4,7 @@ import ProductDetails from './ProductDetails.mjs';
 import { loadHeaderFooter } from './utils.mjs';
 
 const productId = getParam('product');
-console.log('Loaded productId from URL:', productId);
+// console.log('Loaded productId from URL:', productId);
 const dataSource = new ProductData();
 
 const productDetails = new ProductDetails(productId, dataSource);
