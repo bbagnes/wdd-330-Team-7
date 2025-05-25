7f25fbfc755ffb1eea550302994fdbbad4fef7b6
ray24004 <wraymundo@byupathway.edu>
2025-05-24 23:46:44 -0300

    Step 4 - Pulling from an API

--- src/js/ProductData.mjs
+++ src/js/ProductData.mjs
@@ -1,20 +1,19 @@
-function convertToJson(res) {
+const baseURL = import.meta.env.VITE_SERVER_URL;
+
+async function convertToJson(res) {
   if (res.ok) {
-    return res.json();
+    const json = await res.json()
+    return json;
   } else {
     throw new Error('Bad Response');
   }
 }
 
 export default class ProductData {
-  constructor(category) {
-    this.category = category;
-    this.path = `../json/${this.category}.json`;
-  }
-  getData() {
-    return fetch(this.path)
-      .then(convertToJson)
-      .then((data) => data);
+  async getData(category) {
+    const response = await fetch(`${baseURL}products/search/${category}`);
+    const data = await convertToJson(response);
+    return data.Result;
   }
   async findProductById(id) {
     const products = await this.getData();
