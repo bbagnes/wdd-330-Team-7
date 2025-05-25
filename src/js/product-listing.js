5d0bd537b7b9c330a00e53b3d6864e4b8589f352
ray24004 <wraymundo@byupathway.edu>
2025-05-24 23:15:07 -0300

    Step 1 - Refactor

---
+++ src/js/product-listing.js
@@ -0,0 +1,16 @@
+import ProductData from './ProductData.mjs';
+import ProductList from './ProductList.mjs';
+import { loadHeaderFooter } from './utils.mjs';
+
+const category = 'tents';
+const productData = new ProductData(category);
+const productListElement = document.querySelector('.product-list');
+
+const productList = new ProductList(
+  category,
+  productData,
+  productListElement,
+);
+
+loadHeaderFooter();
+productList.init();
