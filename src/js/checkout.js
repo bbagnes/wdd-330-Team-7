aa4d53e5763a2ed4e29ac8dc50869447bc7d1179
ray24004 <wraymundo@byupathway.edu>
2025-06-01 00:06:39 -0300

    Step 4 - Form Validation

--- src/js/checkout.js
+++ src/js/checkout.js
@@ -6,10 +6,27 @@ const checkout = new CheckoutProcess('so-cart', 'main.checkout');
 checkout.init();
 
 // Handle form submission
-document.querySelector("form").addEventListener("submit", function (event) {
-    event.preventDefault();
+document.querySelector('form').addEventListener('submit', function (ev) {
+  ev.preventDefault();
+
+  const form = ev.target;
+  form.reportValidity();
+
+  if (form.checkValidity()) {
     checkout.checkout(this);
+  }
 });
 
 // Load header and footer into the page
 loadHeaderFooter();
+
+// Add autoformat to inputs
+function formatToDigitsOnly(elementEvent) {
+  const input = elementEvent.target;
+  input.value = input.value.replace(/[^0-9/]/g, '');
+}
+
+document.querySelector('#zipcode').addEventListener('input', formatToDigitsOnly);
+document.querySelector('#cardNumber').addEventListener('input', formatToDigitsOnly);
+document.querySelector('#cardExpiration').addEventListener('input', formatToDigitsOnly);
+document.querySelector('#cardCode').addEventListener('input', formatToDigitsOnly);
